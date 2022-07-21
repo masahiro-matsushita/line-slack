f8121cf79155b92175abef506b1a24406d0665ce
masahiro-matsushita <m_matsushita@soune.co.jp>
2022-07-21 22:35:28 +0900

    main.py ブログに記載されている内容に変更

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import slackweb
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
-from linebot.models import MessageEvent, TextMessage, ImageMessage, StickerMessage
+from linebot.models import MessageEvent, TextMessage
 
 app = Flask(__name__)
 
@@ -13,8 +13,6 @@ app = Flask(__name__)
 CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
 CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
 WEB_HOOK_LINKS = os.environ["SLACK_WEB_HOOKS_URL"]
-BOT_OAUTH = os.environ["SLACK_BOT_OAUTH"]
-POST_CHANEL_ID = os.environ["SLACK_POST_CHANEL_ID"]
 
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
@@ -84,67 +82,6 @@ def handle_text_message(event):
     # slack側に投稿するメッセージの加工
     send_msg = "[bot-line] {user_name}さん\n".format(user_name=user_name) \
                + "{msg}\n".format(msg=event.message.text) \
-               + "---\n" \
-               + "送信元: {msg_type} ( {room_id} )\n".format(msg_type=msg_type, room_id=room_id) \
-               + "送信者: {user_name} ( {user_id} )".format(user_name=user_name, user_id=user_id)
-
-    # メッセージの送信
-    slack_info.notify(text=send_msg)
-
-
-@handler.add(MessageEvent, message=ImageMessage)
-def handle_image_message(event):
-    """
-    Image Message の処理
-    """
-
-    # トーク情報の取得
-    user_id, user_name, msg_type, room_id = get_event_info(event)
-
-    # LINEで送信された画像の取得
-    message_id = event.message.id
-    message_content = line_bot_api.get_message_content(message_id)
-    img = message_content.content
-
-    # slack側に投稿するメッセージの加工
-    send_msg = "[bot-line] {user_name}さんが画像を送信しました．\n".format(user_name=user_name) \
-               + "---\n" \
-               + "送信元: {msg_type} ( {room_id} )\n".format(msg_type=msg_type, room_id=room_id) \
-               + "送信者: {user_name} ( {user_id} )".format(user_name=user_name, user_id=user_id)
-
-    file_name = "send_image_{message_id}".format(message_id=message_id)
-
-    # 画像送信
-    files = {'file': img}
-    param = {
-        'token': BOT_OAUTH,
-        'channels': POST_CHANEL_ID,
-        'filename': file_name,
-        'initial_comment': send_msg,
-        'title': file_name
-    }
-    response = requests.post(url="https://slack.com/api/files.upload", params=param, files=files)
-
-
-@handler.add(MessageEvent, message=StickerMessage)
-def handle_sticker_message(event):
-    """
-    Sticker Message の処理
-    """
-
-    slack_info = slackweb.Slack(url=WEB_HOOK_LINKS)
-
-    # トーク情報の取得
-    user_id, user_name, msg_type, room_id = get_event_info(event)
-
-    # LINEで送信されたスタンプ情報の取得
-    package_id = event.message.package_id
-    sticker_id = event.message.sticker_id
-
-    # slack側に投稿するメッセージの加工
-    send_msg = "[bot-line] {user_name}さんがスタンプを送信しました．\n".format(user_name=user_name) \
-               + "package_id: {package_id}\n".format(package_id=package_id) \
-               + "sticker_id: {sticker_id}\n".format(sticker_id=sticker_id) \
                + "---\n" \
                + "送信元: {msg_type} ( {room_id} )\n".format(msg_type=msg_type, room_id=room_id) \
                + "送信者: {user_name} ( {user_id} )".format(user_name=user_name, user_id=user_id)
